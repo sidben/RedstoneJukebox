1ce19795641b71d148cf1f693fe076bce95e083b
sidben <noemail>
2012-12-17 18:24:04 -0200

    Fixed lightning bug

--- src/sidben/redstonejukebox/client/RenderRedstoneJukebox.java
+++ src/sidben/redstonejukebox/client/RenderRedstoneJukebox.java
@@ -4,11 +4,9 @@ package sidben.redstonejukebox.client;
 import org.lwjgl.opengl.GL11;
 
 import sidben.redstonejukebox.ModRedstoneJukebox;
-import net.minecraft.src.Block;
-import net.minecraft.src.IBlockAccess;
-import net.minecraft.src.RenderBlocks;
-import net.minecraft.src.Tessellator;
+import net.minecraft.src.*;
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
+import cpw.mods.fml.common.FMLCommonHandler;
 
 
 
@@ -17,7 +15,7 @@ public class RenderRedstoneJukebox implements ISimpleBlockRenderingHandler {
 
 	@Override
 	public void renderInventoryBlock(Block block, int metadata, int modelID, RenderBlocks renderblocks) {
-
+		
         Tessellator tessellator = Tessellator.instance;
 
 
@@ -84,11 +82,36 @@ public class RenderRedstoneJukebox implements ISimpleBlockRenderingHandler {
 
 	@Override
 	public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderblocks) {
+		
 
-		// Disc face
-		renderblocks.renderTopFace(		block, (double)x, (double)(y-0.251), (double)z, ModRedstoneJukebox.texJukeboxDisc);
+        // Copied from renderBlockCauldron, this have something to do with the correct lightning of the block
+        renderblocks.renderStandardBlock(block, x, y, z);
 
-		// Other faces
+        
+        Tessellator tessellator = Tessellator.instance;
+        tessellator.setBrightness(block.getMixedBrightnessForBlock(world, x, y, z));
+        float var6 = 1.0F;
+        int var7 = block.colorMultiplier(world, x, y, z);
+        float var8 = (float)(var7 >> 16 & 255) / 255.0F;
+        float var9 = (float)(var7 >> 8 & 255) / 255.0F;
+        float var10 = (float)(var7 & 255) / 255.0F;
+        float var12;
+
+        if (EntityRenderer.anaglyphEnable)
+        {
+            float var11 = (var8 * 30.0F + var9 * 59.0F + var10 * 11.0F) / 100.0F;
+            var12 = (var8 * 30.0F + var9 * 70.0F) / 100.0F;
+            float var13 = (var8 * 30.0F + var10 * 70.0F) / 100.0F;
+            var8 = var11;
+            var9 = var12;
+            var10 = var13;
+        }
+
+        tessellator.setColorOpaque_F(var6 * var8, var6 * var9, var6 * var10);
+
+        
+        
+		// Regular faces
 		renderblocks.renderBottomFace(	block, (double)x, (double)y, (double)z, block.getBlockTexture(world, x, y, z, 0));
 		renderblocks.renderTopFace(		block, (double)x, (double)y, (double)z, block.getBlockTexture(world, x, y, z, 1));
 		renderblocks.renderEastFace(	block, (double)x, (double)y, (double)z, block.getBlockTexture(world, x, y, z, 2));
@@ -96,7 +119,10 @@ public class RenderRedstoneJukebox implements ISimpleBlockRenderingHandler {
 		renderblocks.renderNorthFace(	block, (double)x, (double)y, (double)z, block.getBlockTexture(world, x, y, z, 4));
 		renderblocks.renderSouthFace(	block, (double)x, (double)y, (double)z, block.getBlockTexture(world, x, y, z, 5));
 
-
+		// Disc face
+		renderblocks.renderTopFace(		block, (double)x, (double)(y-0.251), (double)z, block.getBlockTexture(world, x, y, z, 7));		// 7 = my special texture
+		
+		
 		return true;
 	}
 

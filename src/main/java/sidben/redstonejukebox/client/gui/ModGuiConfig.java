775afa5bb409504223f2edfea3c7510ab658df8d
sidben <sidbenetti@gmail.com>
2015-09-11 00:43:19 -0300

    Config GUI localization
    Pretty names! In order!

--- src/main/java/sidben/redstonejukebox/client/gui/ModGuiConfig.java
+++ src/main/java/sidben/redstonejukebox/client/gui/ModGuiConfig.java
@@ -5,7 +5,6 @@ import sidben.redstonejukebox.reference.Reference;
 import cpw.mods.fml.client.config.GuiConfig;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.common.config.ConfigElement;
-import net.minecraftforge.common.config.Configuration;
 
 
 public class ModGuiConfig extends GuiConfig {
@@ -13,7 +12,7 @@ public class ModGuiConfig extends GuiConfig {
 
     public ModGuiConfig(GuiScreen guiScreen)
     {
-        super(guiScreen, new ConfigElement<Object>(ConfigurationHandler.config.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(), Reference.ModID, false, false, GuiConfig.getAbridgedConfigPath(ConfigurationHandler.config.toString()));
+        super(guiScreen, new ConfigElement<Object>(ConfigurationHandler.config.getCategory(ConfigurationHandler.CATEGORY_TRADING)).getChildElements(), Reference.ModID, false, false, GuiConfig.getAbridgedConfigPath(ConfigurationHandler.config.toString()));
     }
 
 }
--- src/main/java/sidben/redstonejukebox/handler/ConfigurationHandler.java
+++ src/main/java/sidben/redstonejukebox/handler/ConfigurationHandler.java
@@ -1,10 +1,13 @@
 package sidben.redstonejukebox.handler;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import sidben.redstonejukebox.reference.Reference;
 import cpw.mods.fml.client.event.ConfigChangedEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
 
 
 public class ConfigurationHandler
@@ -15,34 +18,49 @@ public class ConfigurationHandler
      * this parameters, check sidben.redstonejukebox.helper.RecordStoreHelper.
      * 
      */
+    
+    public static final String CATEGORY_TRADING = "record_trading";
+    public static final String CATEGORY_CUSTOM = "custom_records"; 
+
+    
 
     /** Maximum amount of record trade lists (also called stores) */
-    public static int maxStores = 256;
+    public static int maxStores;
     
     /** Time in minutes a store will remain in cache */
-    public static int expirationTime = 20;
+    public static int expirationTime;
     
     /** Maximum amount of new record trades a villager can get */
-    public static int maxExtraOffers = 3;
+    public static int maxExtraOffers;
     
     /** Maximum amount of times a record trade can be used */
-    public static int maxTrades = 3;
+    public static int maxTrades;
     
     /** Minimum price in emeralds that a villager will pay when buying records from the player */
-    public static int recordPriceBuyMin = 5;
+    public static int recordPriceBuyMin;
     
     /** Maximum price in emeralds that a villager will pay when buying records from the player */
-    public static int recordPriceBuyMax = 11;
+    public static int recordPriceBuyMax;
     
     /** Minimum price in emeralds that a villager will charge when selling records to players */
-    public static int recordPriceSellMin = 8;
+    public static int recordPriceSellMin;
 
     /** Maximum price in emeralds that a villager will charge when selling records to players */
-    public static int recordPriceSellMax = 15;
+    public static int recordPriceSellMax;
     
     /** Ratio of buying records VS selling records trades. By default, 60% of the offers will be to buy records from players. */
     public static final int buyingOffersRatio = 60;
 
+    private static final int DEFAULT_maxStores              = 256;
+    private static final int DEFAULT_expirationTime         = 20;
+    private static final int DEFAULT_maxExtraOffers         = 3;
+    private static final int DEFAULT_maxTrades              = 3;
+    private static final int DEFAULT_recordPriceBuyMin      = 5;
+    private static final int DEFAULT_recordPriceBuyMax      = 11;
+    private static final int DEFAULT_recordPriceSellMin     = 8;
+    private static final int DEFAULT_recordPriceSellMax     = 15;
+    
+    
     
     
     
@@ -67,16 +85,57 @@ public class ConfigurationHandler
 
     private static void loadConfig()
     {
-        // Load properties
-        maxStores           = config.getInt("max_stores", Configuration.CATEGORY_GENERAL, 256, 16, 1024, "Maximum amount of record trading lists the server will keep in memory.");
-        expirationTime      = config.getInt("expiration_store", Configuration.CATEGORY_GENERAL, 20, 1, 1440, "Time in minutes that a record trading list will be kept in memory (cached).");
-        maxExtraOffers      = config.getInt("max_new_trades", Configuration.CATEGORY_GENERAL, 3, 1, 999, "Maximum amount of new record trades a villager can get.");
-        maxTrades           = config.getInt("record_trade_count", Configuration.CATEGORY_GENERAL, 3, 1, 999, "Amount of times each record trade can be used.");
+        List<String> propOrder = new ArrayList<String>();
+        Property prop;
+
         
-        recordPriceBuyMin   = config.getInt("record_buy_price_min", Configuration.CATEGORY_GENERAL, 5, 1, 64, "Minimum price a villager will pay when buying records.");
-        recordPriceBuyMax   = config.getInt("record_buy_price_max", Configuration.CATEGORY_GENERAL, 11, 1, 64, "Maximum price a villager will pay when buying records.");
-        recordPriceSellMin  = config.getInt("record_sell_price_min", Configuration.CATEGORY_GENERAL, 8, 1, 64, "Minimum price a villager will charge when selling records.");
-        recordPriceSellMax  = config.getInt("record_sell_price_max", Configuration.CATEGORY_GENERAL, 15, 1, 64, "Maximum price a villager will charge when selling records.");
+        // Load properties
+        prop = config.get(CATEGORY_TRADING, "max_stores", DEFAULT_maxStores, "", 16, 1024);
+        prop.setLanguageKey("sidben.redstonejukebox.configgui.max_stores");
+        maxStores = prop.getInt(DEFAULT_maxStores);
+        propOrder.add(prop.getName());
+
+        prop = config.get(CATEGORY_TRADING, "expiration_store", DEFAULT_expirationTime, "", 1, 1440);
+        prop.setLanguageKey("sidben.redstonejukebox.configgui.expiration_store");
+        expirationTime = prop.getInt(DEFAULT_expirationTime);
+        propOrder.add(prop.getName());
+
+        prop = config.get(CATEGORY_TRADING, "max_new_trades", DEFAULT_maxExtraOffers, "", 1, 999);
+        prop.setLanguageKey("sidben.redstonejukebox.configgui.max_new_trades");
+        maxExtraOffers = prop.getInt(DEFAULT_maxExtraOffers);
+        propOrder.add(prop.getName());
+
+        prop = config.get(CATEGORY_TRADING, "record_trade_count", DEFAULT_maxTrades, "", 1, 999);
+        prop.setLanguageKey("sidben.redstonejukebox.configgui.record_trade_count");
+        maxTrades = prop.getInt(DEFAULT_maxTrades);
+        propOrder.add(prop.getName());
+
+        
+        
+        prop = config.get(CATEGORY_TRADING, "record_buy_price_min", DEFAULT_recordPriceBuyMin, "", 1, 64);
+        prop.setLanguageKey("sidben.redstonejukebox.configgui.record_buy_price_min");
+        recordPriceBuyMin = prop.getInt(DEFAULT_recordPriceBuyMin);
+        propOrder.add(prop.getName());
+
+        prop = config.get(CATEGORY_TRADING, "record_buy_price_max", DEFAULT_recordPriceBuyMax, "", 1, 64);
+        prop.setLanguageKey("sidben.redstonejukebox.configgui.record_buy_price_max");
+        recordPriceBuyMax = prop.getInt(DEFAULT_recordPriceBuyMax);
+        propOrder.add(prop.getName());
+
+        prop = config.get(CATEGORY_TRADING, "record_sell_price_min", DEFAULT_recordPriceSellMin, "", 1, 64);
+        prop.setLanguageKey("sidben.redstonejukebox.configgui.record_sell_price_min");
+        recordPriceSellMin = prop.getInt(DEFAULT_recordPriceSellMin);
+        propOrder.add(prop.getName());
+
+        prop = config.get(CATEGORY_TRADING, "record_sell_price_max", DEFAULT_recordPriceSellMax, "", 1, 64);
+        prop.setLanguageKey("sidben.redstonejukebox.configgui.record_sell_price_max");
+        recordPriceSellMax = prop.getInt(DEFAULT_recordPriceSellMax);
+        propOrder.add(prop.getName());
+
+        
+        
+        config.setCategoryPropertyOrder(CATEGORY_TRADING, propOrder);
+
 
         
         // saving the configuration to its file

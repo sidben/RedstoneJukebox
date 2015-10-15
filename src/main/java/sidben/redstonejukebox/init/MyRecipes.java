fd8e89d3ade51653530e1258d69d27a30929f37a
sidben <sidbenetti@gmail.com>
2015-10-14 22:21:35 -0300

    Integration with Forge ore dictionary (possible fix for #17)

--- src/main/java/sidben/redstonejukebox/init/MyRecipes.java
+++ src/main/java/sidben/redstonejukebox/init/MyRecipes.java
@@ -4,6 +4,8 @@ import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 
@@ -15,46 +17,22 @@ public class MyRecipes
     public static void register()
     {
 
-        final Block woodStack = Blocks.planks;
         final ItemStack redstoneTorchStack = new ItemStack(Blocks.redstone_torch);
-        final ItemStack glassStack = new ItemStack(Blocks.glass);
         final ItemStack jukeboxStack = new ItemStack(Blocks.jukebox);
         final ItemStack flintStack = new ItemStack(Items.flint);
         final ItemStack redstoneStack = new ItemStack(Items.redstone);
 
-        final ItemStack recordStack1 = new ItemStack(Items.record_11);
-        final ItemStack recordStack2 = new ItemStack(Items.record_13);
-        final ItemStack recordStack3 = new ItemStack(Items.record_blocks);
-        final ItemStack recordStack4 = new ItemStack(Items.record_cat);
-        final ItemStack recordStack5 = new ItemStack(Items.record_chirp);
-        final ItemStack recordStack6 = new ItemStack(Items.record_far);
-        final ItemStack recordStack7 = new ItemStack(Items.record_mall);
-        final ItemStack recordStack8 = new ItemStack(Items.record_mellohi);
-        final ItemStack recordStack9 = new ItemStack(Items.record_stal);
-        final ItemStack recordStack10 = new ItemStack(Items.record_strad);
-        final ItemStack recordStack11 = new ItemStack(Items.record_wait);
-        final ItemStack recordStack12 = new ItemStack(Items.record_ward);
 
         final ItemStack redstoneJukeboxStack = new ItemStack(MyBlocks.redstoneJukebox);
         final ItemStack blankRecordStack = new ItemStack(MyItems.recordBlank, 1);
 
 
         // Redstone Jukebox
-        GameRegistry.addRecipe(redstoneJukeboxStack, "ggg", "tjt", "www", 'g', glassStack, 't', redstoneTorchStack, 'j', jukeboxStack, 'w', woodStack);
+        GameRegistry.addRecipe(new ShapedOreRecipe(redstoneJukeboxStack, "ggg", "tjt", "www", 'g', "blockGlass", 't', redstoneTorchStack, 'j', jukeboxStack, 'w', "plankWood"));
 
         // Blank Record
-        GameRegistry.addShapelessRecipe(blankRecordStack, recordStack1, flintStack, redstoneStack);
-        GameRegistry.addShapelessRecipe(blankRecordStack, recordStack2, flintStack, redstoneStack);
-        GameRegistry.addShapelessRecipe(blankRecordStack, recordStack3, flintStack, redstoneStack);
-        GameRegistry.addShapelessRecipe(blankRecordStack, recordStack4, flintStack, redstoneStack);
-        GameRegistry.addShapelessRecipe(blankRecordStack, recordStack5, flintStack, redstoneStack);
-        GameRegistry.addShapelessRecipe(blankRecordStack, recordStack6, flintStack, redstoneStack);
-        GameRegistry.addShapelessRecipe(blankRecordStack, recordStack7, flintStack, redstoneStack);
-        GameRegistry.addShapelessRecipe(blankRecordStack, recordStack8, flintStack, redstoneStack);
-        GameRegistry.addShapelessRecipe(blankRecordStack, recordStack9, flintStack, redstoneStack);
-        GameRegistry.addShapelessRecipe(blankRecordStack, recordStack10, flintStack, redstoneStack);
-        GameRegistry.addShapelessRecipe(blankRecordStack, recordStack11, flintStack, redstoneStack);
-        GameRegistry.addShapelessRecipe(blankRecordStack, recordStack12, flintStack, redstoneStack);
+        GameRegistry.addRecipe(new ShapelessOreRecipe(blankRecordStack, "record", flintStack, redstoneStack));
+
 
     }
 

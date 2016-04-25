8b8b45dffa9ecd3bdc3b0c8498acfb5e119301aa
sidben <sidbenetti@gmail.com>
2016-04-25 14:00:49 -0300

    1.8 port - changed mod id
    Had to change this or else my mod won't find the JSON files for blocks and item rendering.

--- src/main/java/sidben/redstonejukebox/reference/Reference.java
+++ src/main/java/sidben/redstonejukebox/reference/Reference.java
@@ -3,11 +3,11 @@ package sidben.redstonejukebox.reference;
 public class Reference 
 {
 
-    public static final String  ModID              	= "SidbenRedstoneJukebox";
+    public static final String  ModID              	= "redstonejukebox";
     public static final String  ModName            	= "Redstone Jukebox";
     public static final String  ModVersion         	= "${version}";
     public static final String  ModChannel          = "CH_RSJUKEBOX";
-    public static final String  ResourcesNamespace  = "redstonejukebox";
+    public static final String  ResourcesNamespace  = "redstonejukebox";			// TODO: replace for ModID. This was a workaround anyway
 
     public static final String  ServerProxyClass   	= "sidben.redstonejukebox.proxy.ServerProxy";
     public static final String  ClientProxyClass   	= "sidben.redstonejukebox.proxy.ClientProxy";
